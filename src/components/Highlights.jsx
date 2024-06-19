d4638f58c10bc53d042bab70f6dfb123e462f881
Nandhu125 <96743168+Nandhu125@users.noreply.github.com>
2024-06-19 17:07:00 +0530

    code added

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <div onClick={(
+      () => {
+        alert('Please select')
+      }
+    )}>Highlights</div>
+  )
+}
+
+export default Highlights
